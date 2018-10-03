dca782a20a02156e28af2be0c0a328c130cf3bb6
Matt Stewart <themattstewart@gmail.com>
2018-10-03 16:22:19 -0700

    add spline interpolation from lab 3

---
+++ lab03/include/Paths.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace lab3
+{
+    constexpr const char ShaderDirectory[] = "/Users/stewartm/Downloads/csc473_fall2018_labs-master/labs/labs/lab03/shaders/";
+    constexpr const char DataDirectory[] = "/Users/stewartm/Downloads/csc473_fall2018_labs-master/labs/data/";
+}
